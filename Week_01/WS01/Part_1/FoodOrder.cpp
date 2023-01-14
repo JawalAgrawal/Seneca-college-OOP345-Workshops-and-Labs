f368350e72a5be778acf17e75812b3e5b738b428
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-14 11:49:21 -0500

    Added basic function prototypes

--- Week_01/WS01/Part_1/FoodOrder.cpp
+++ Week_01/WS01/Part_1/FoodOrder.cpp
@@ -1,5 +1,33 @@
 #include "FoodOrder.h"
 
-namespace sdds {
+#include <iostream>
+using namespace std;
 
+namespace sdds {
+    FoodOrder::FoodOrder() {
+        setEmpty();
+    }
+
+    std::istream& FoodOrder::read(std::istream& is) {
+        if (is) {
+//            <Customer Name>,<Order Description>,<Price>,<Daily Special Status>[newline]
+            string name;
+            string orderDescription;
+            string price;
+            string dailySpecialStatus;
+            string line;
+        }
+        return is;
+    }
+
+    void FoodOrder::display() const {
+        cout << "2 . Julian    |8 Piece Sushi Set        |9.03        |         7.88" << endl;
+    }
+
+    void FoodOrder::setEmpty() {
+        customerName[0] = '\0';
+        foodDescription[0] = '\0';
+        foodPrice = 0;
+        dailySpecial = false;
+    }
 }
--- Week_01/WS01/Part_1/FoodOrder.h
+++ Week_01/WS01/Part_1/FoodOrder.h
@@ -1,9 +1,25 @@
 #ifndef SDDS_FOODORDER_H
 #define SDDS_FOODORDER_H
 
+#include <iostream>
+
+double g_taxrate;
+double g_dailydiscount;
+
 namespace sdds {
     class FoodOrder {
+    private:
+        char customerName[10];
+        char foodDescription[25];
+        double foodPrice;
+        bool dailySpecial;
 
+        void setEmpty();
+
+    public:
+        FoodOrder();
+        std::istream& read(std::istream& is);
+        void display() const;
     };
 }
 
