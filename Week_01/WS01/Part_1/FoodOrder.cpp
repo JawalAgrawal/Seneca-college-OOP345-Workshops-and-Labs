441deaf5014815332d58d9ead581f170761956d5
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-14 13:34:17 -0500

    Fixed food order reading bug

--- Week_01/WS01/Part_1/FoodOrder.cpp
+++ Week_01/WS01/Part_1/FoodOrder.cpp
@@ -1,5 +1,5 @@
 #include "FoodOrder.h"
-#include <sstream>
+#include <string>
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -15,17 +15,38 @@ namespace sdds {
 
     std::istream& FoodOrder::read(std::istream& is) {
         if (is) {
+
+            // Reading the customer name
             char temp[1024];
             is.getline(temp, 1024, ',');
 
-//            if (customerName) {
-//                delete[] customerName;
-//                customerName = nullptr;
-//            }
-
-            if (is) {
+            if (strlen(temp) > 0) {
                 customerName = new char[strlen(temp) + 1];
                 strcpy(customerName, temp);
+
+                // Reading the Food Description
+                char temp2[1024];
+                is.getline(temp2, 1024, ',');
+
+                foodDescription = new char[strlen(temp2) + 1];
+                strcpy(foodDescription, temp2);
+
+                // Reading the food price
+                is >> foodPrice;
+                is.ignore(1);
+
+                // Reading whether there is any special pricing
+                char temp3[1024];
+                is.getline(temp3, 1024, '\n');
+
+                if (temp3[0] == 'N') {
+                    dailySpecial = false;
+                } else if (temp3[0] == 'Y') {
+                    dailySpecial = true;
+                }
+            } else {
+                cout << "No Order" << endl;
+                setEmpty();
             }
 
         }
@@ -33,9 +54,9 @@ namespace sdds {
     }
 
     void FoodOrder::display() const {
-//        cout << customerName << foodDescription << foodPrice << dailySpecial << endl;
-        cout << customerName << endl;
-
+        if (customerName != nullptr) {
+            cout << customerName << " " << foodDescription << " " << foodPrice << " " << dailySpecial << endl;
+        }
     }
 
     void FoodOrder::setEmpty() {
