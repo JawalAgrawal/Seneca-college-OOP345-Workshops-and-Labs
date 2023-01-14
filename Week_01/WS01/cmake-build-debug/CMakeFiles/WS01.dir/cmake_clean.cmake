5fb2b3fcc8567143b0efdd1f22d605e93ba229bd
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-14 14:38:17 -0500

    WS Draft Uploaded

---
+++ Week_01/WS01/cmake-build-debug/CMakeFiles/WS01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WS01.dir/FoodOrder.cpp.o"
+  "CMakeFiles/WS01.dir/FoodOrder.cpp.o.d"
+  "CMakeFiles/WS01.dir/w1_p1.cpp.o"
+  "CMakeFiles/WS01.dir/w1_p1.cpp.o.d"
+  "WS01"
+  "WS01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WS01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
