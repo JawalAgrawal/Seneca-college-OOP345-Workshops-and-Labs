e0fde808f9c4657220f1d0698983fc7de6e111ab
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-21 07:34:00 -0500

    Added function prototypes

--- Week_02/WS02/part 1/TimedTask.h
+++ Week_02/WS02/part 1/TimedTask.h
@@ -4,9 +4,33 @@
 #include <iostream>
 
 namespace sdds {
-    class TimedTask {
-
+    struct Task {
+        std::string taskName;
+        std::string unitOfTime;
+        std::chrono::duration<int> seconds_type;
     };
+    class TimedTask {
+    private:
+        int numberOfRecordsStored;
+        std::chrono::time_point<std::chrono::steady_clock> taskStartTime;
+        std::chrono::time_point<std::chrono::steady_clock> taskEndTime;
+        Task* taskArray;
+
+    public:
+        TimedTask();
+
+        // A modifier that starts the timer for an event
+        void startClock();
+
+        // A modifier that stops the timer for an event
+        void stopClock();
+
+        // This function will update the next time-record in the array
+        void addTask();
+    };
+
+    // Should insert in the std::ostream object the records from the array
+    std::ostream& operator<<(std::ostream& os, const TimedTask& timedTask);
 }
 
 #endif // SDDS_TIMEDTASK_H
