9397a4bba13ce8c8b75681db1f0c5dfb286ea40a
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-21 09:20:36 -0500

    Added basic definitions

--- Week_02/WS02/part 1/ProteinDatabase.cpp
+++ Week_02/WS02/part 1/ProteinDatabase.cpp
@@ -1,8 +1,51 @@
 #include "ProteinDatabase.h"
 #include <iostream>
 #include <cstring>
+#include <fstream>
 using namespace std;
 
 namespace sdds {
+    ProteinDatabase::ProteinDatabase() {
+        setEmpty();
+    }
 
+    // Populates the current object n
+    ProteinDatabase::ProteinDatabase(const char* filename) {
+        // Buffer to read text into
+        string proteinSequence;
+        ifstream MyReadFile(filename);
+
+        // Use a while loop together with the getline() function to read the file line by line
+        while (getline (MyReadFile, proteinSequence)) {
+            // Checking to only read sequences and not the description
+            if (proteinSequence[0] != '>') {
+                cout << proteinSequence << endl;
+            }
+        }
+
+        // Close the file
+        MyReadFile.close();
+    }
+
+    // Deallocates dynamic memory
+    ProteinDatabase::~ProteinDatabase() {
+        delete[] proteinSeqArr;
+        proteinSeqArr = nullptr;
+    }
+
+    // Returns the number of protein sequences stored in the current object
+    size_t ProteinDatabase::size() const {
+        return arrSize;
+    }
+
+    // Returns a copy of the protein sequence at the index received as the function parameter
+    std::string ProteinDatabase::operator[](size_t index) const {
+        return proteinSeqArr[index];
+    }
+
+    // Initializes to empty state
+    void ProteinDatabase::setEmpty() {
+        arrSize = 0;
+        proteinSeqArr = nullptr;
+    }
 }
--- Week_02/WS02/part 1/ProteinDatabase.h
+++ Week_02/WS02/part 1/ProteinDatabase.h
@@ -5,7 +5,26 @@
 
 namespace sdds {
     class ProteinDatabase {
+    private:
+        int arrSize;
+        std::string* proteinSeqArr;
+    public:
+        ProteinDatabase();
 
+        // Populates the current object
+        ProteinDatabase(const char* filename);
+
+        // Deallocates dynamic memory
+        ~ProteinDatabase();
+
+        // Returns the number of protein sequences stored in the current object
+        size_t size() const;
+
+        // Returns a copy of the protein sequence at the index received as the function parameter
+        std::string operator[](size_t index) const;
+
+        // Initializes to empty state
+        void setEmpty();
     };
 }
 
