e716625ef5199bd9c005e77fa12e81418786a516
Aryan Khurana <aryankhurana1511@gmail.com>
2023-01-31 07:14:13 -0500

    Added headers

--- Week_03/WS03/part 1/Collection.h
+++ Week_03/WS03/part 1/Collection.h
@@ -1,3 +1,14 @@
+/*
+*****************************************************************************
+                              Collection.h
+Full Name  : Aryan Khurana
+Student ID#: 145282216
+Email      : akhurana22@myseneca.ca
+Date of completion    : 01 February 2023
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
 #ifndef SDDS_COLLECTION_H
 #define SDDS_COLLECTION_H
 
