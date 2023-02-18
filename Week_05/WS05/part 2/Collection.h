32f65314b685f0fe0b5c6737baca849e6a44b89c
Aryan Khurana <aryankhurana1511@gmail.com>
2023-02-18 17:46:10 -0500

    Changed dates, solved bugs and added collection module starter code

--- Week_05/WS05/part 2/Collection.h
+++ Week_05/WS05/part 2/Collection.h
@@ -1,6 +1,6 @@
 /*
 *****************************************************************************
-                              Movie.cpp
+                              Collection.h
 Full Name  : Aryan Khurana
 Student ID#: 145282216
 Email      : akhurana22@myseneca.ca
@@ -9,9 +9,13 @@ I have done all the coding by myself and only copied the code that my professor 
 *****************************************************************************
 */
 
-#include "Movie.h"
-#include <iostream>
+#ifndef SDDS_COLLECTION_H
+#define SDDS_COLLECTION_H
 
 namespace sdds {
+    class Collection {
 
+    };
 }
+
+#endif // SDDS_COLLECTION_H
